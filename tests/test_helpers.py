d1b418b270d26b7b96cdac05e5cf545e03457d52
Brandon <bmwalshy@gmail.com>
2021-09-12 13:28:30 -0400

    Add tests for is_production() helper

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,7 @@
 import pytest
 from todo_or_die.helpers.generate_message import generate_message
+from todo_or_die.helpers.is_production import is_production
+
 
 def test_generates_message_without_date():
   result = generate_message("Message without date")
@@ -11,3 +13,25 @@ def test_generates_message_with_date():
 
   assert result == "TODO, 'Message without date', was not completed by 2021-09-08. Get on this!"
 
+def test_not_production_env():
+  assert is_production() == False
+
+def test_production_env(monkeypatch):
+    monkeypatch.setenv('PRODUCTION', 'true')
+    assert is_production() is True
+    monkeypatch.delenv('PRODUCTION')
+
+def test_django_env(monkeypatch):
+    monkeypatch.setenv('DJANGO_ENVIRONMENT', 'production')
+    assert is_production() is True
+    monkeypatch.delenv('DJANGO_ENVIRONMENT')
+
+def test_environment_env(monkeypatch):
+    monkeypatch.setenv('ENVIRONMENT', 'production')
+    assert is_production() is True
+    monkeypatch.delenv('ENVIRONMENT')
+
+def test_flask_env(monkeypatch):
+    monkeypatch.setenv('FLASK_ENV', 'production')
+    assert is_production() is True
+    monkeypatch.delenv('FLASK_ENV')
